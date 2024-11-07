1aab95f247e1b748f68e097d7302b78e367f1bc7
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-11-07 13:52:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ import pandas as pd
 import random
 import time
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Cargar los archivos .xlsx
 @st.cache_resource
 def cargar_datos():
